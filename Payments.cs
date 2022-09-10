a05dc69cd75a16144e8846053aba0cda6e110f27
unknown <azazy.ehab@gmail.com>
2022-09-10 22:57:37 +0200

    Interface

---
+++ Payments.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFace
+{
+    class Cashier
+    {
+        private IPayment Payment { get; set; }
+        public Cashier(IPayment _payment)
+        {
+            this.Payment = _payment;
+        }
+        public void CheckPayment(decimal _cash)
+        {
+            Payment.Pay(_cash);
+        }
+    }
+    class Cash : IPayment
+    {
+        public void Pay(decimal _cash)
+        {
+            Console.WriteLine($"{this.GetType().Name} Payment: {Math.Round(_cash, 2):N0}");
+        }
+    }
+    class Visa : IPayment
+    {
+        public void Pay(decimal _cash)
+        {
+            Console.WriteLine($"{this.GetType().Name} Payment: {Math.Round(_cash, 2):N0}");
+        }
+    }
+    class MasterCard : IPayment
+    {
+        public void Pay(decimal _cash)
+        {
+            Console.WriteLine($"{this.GetType().Name} Payment: {Math.Round(_cash, 2):N0}");
+        }
+    }
+    class Debit : IPayment
+    {
+        public void Pay(decimal _cash)
+        {
+            Console.WriteLine($"{this.GetType().Name} Payment: {Math.Round(_cash, 2):N0}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "AzZazi!";
+            Console.ForegroundColor = ConsoleColor.Green;
+            //////////////////////////////////////////////////
+
+            Cash C = new Cash();
+            MasterCard M = new MasterCard();
+            Debit D = new Debit();
+            Visa V = new Visa();
+
+            Cashier Cash = new Cashier(C);
+            Cashier Cash1 = new Cashier(M);
+            Cashier Cash2 = new Cashier(D);
+            Cashier Cash3 = new Cashier(V);
+            /// <summary>
+            /// Or Cashier Cash = new Cashier(new Cash); //Directly Like Anonymous Object.
+            /// </summary>
+            Cash.CheckPayment(99999.94m);
+            Cash1.CheckPayment(99999.94m);
+            Cash2.CheckPayment(99999.94m);
+            Cash3.CheckPayment(99999.94m);
+        }
+    }
+}
